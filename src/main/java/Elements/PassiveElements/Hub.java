8b821246f19c36d61bc6f2fa8f2c8fde47dc39ba
NFetissow <nfetissow@gmail.com>
2015-03-09 14:54:16 +0300

    Hub class

--- src/main/java/Elements/PassiveElements/Hub.java
+++ src/main/java/Elements/PassiveElements/Hub.java
@@ -1,9 +1,39 @@
 package Elements.PassiveElements;
 
 import Elements.PassiveElements.PassiveElement;
+import Elements.PathElement;
 
-/**
- * Created by Никита on 08.03.2015.
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+
+/***
+ * Hub can be connected to many elements
  */
 public class Hub extends PassiveElement {
+    private HashSet<PathElement> connectedElements;
+
+    public Hub() {
+        connectedElements = new HashSet<PathElement>();
+    }
+
+    public void addConnection(PathElement element) {
+        this.connectedElements.add(element);
+    }
+
+    public void disconnectElement(PathElement element) {
+        this.connectedElements.remove(element);
+    }
+
+    @Override
+    public Collection<PathElement> getConnections() {
+        ArrayList<PathElement> connections = new ArrayList<PathElement>();
+        for(PathElement element: connectedElements) {
+            if(element != null) {
+                connections.add(element);
+            }
+        }
+        return connections;
+    }
 }
