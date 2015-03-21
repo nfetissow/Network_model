148a8082fda87929d819c5cefc382dcc6286b834
NFetissow <nfetissow@gmail.com>
2015-03-21 15:26:00 +0300

    Added costs Matrix genereation

--- src/main/java/Networks/NetWork.java
+++ src/main/java/Networks/NetWork.java
@@ -3,6 +3,7 @@ package Networks;
 import Elements.PathElement;
 
 import java.util.HashMap;
+import java.util.List;
 
 
 /***
@@ -10,7 +11,7 @@ import java.util.HashMap;
  *
  */
 public interface NetWork {
-    public HashMap<Object, PathElement> getPathElements();
+    public List<PathElement> getPathElements();
 
 
 }
--- src/main/java/RouteProviders/RouteProviderImpl.java
+++ src/main/java/RouteProviders/RouteProviderImpl.java
@@ -17,13 +17,26 @@ public class RouteProviderImpl implements RouteProvider {
 
     @Override
     public List<PathElement> getRoute(Integer firstID, Integer secondID, NetWork net) throws RouteNotFoundException {
-        Object[] elements = net.getPathElements().values().toArray();
 
-        for(PathElement element : elements) {
-          //Filling value matrix
-          Collection<PathElement> neighbors = element.getConnections();
+        PathElement[] elements = null;
+        net.getPathElements().toArray(elements);
+        double[][] costsMatrix = new double[elements.length][elements.length];
 
+        //Filling costsMatrix with values
+        PathElement element;
+        for (int i = 0; i < elements.length; i++) {
+            element = elements[i];
+            for(PathElement connectedElement: element.getConnections()) {
+                for (int j = 0; j < elements.length; j++) {
+                    if(connectedElement.equals(elements[j])) {
+                        costsMatrix[i][j] = connectedElement.getCosts();
+                        break;
+                    }
+                }
+            }
         }
+
+
 
         //Then using matrix we use Dijkstra's algorithm to find a route;
 
